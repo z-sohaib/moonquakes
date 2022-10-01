27084c295a9bc994d1d8b579da261911d613b1df
YousraFarhani <jy_farhani@esi.dz>
2022-10-01 18:58:59 +0100

    hey

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,8 +9,8 @@ function Home() {
 	return (
 		<div className="flex h-full justify-between gap-x-16 items-center bg-cover">
 			<div className="text-white flex flex-col justify-center items-start gap-y-8">
-				<p className="text-[42px] font-semibold">Visualize MoonQuakes...</p>
-				<p className="text-[24px]">Did you knew that also moon can have quakes on its surface?  🤔  Get to discover that now !!</p>
+				<p className="text-[60px] font-semibold">Visualize MoonQuakes...</p>
+				<p className="text-[22px] ">Did you knew that also moon can have quakes on its surface?  🤔  Get to discover that now !!</p>
 				<div className="glass flex justify-center items-center self-center py-3 px-8 gap-x-4">
 					<p>Rotate the moon</p>
 					<img src="/images/main.svg" alt="main" />
