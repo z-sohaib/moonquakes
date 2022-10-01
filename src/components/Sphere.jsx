2427cc6f60ffb47fcf938614998ec804e4904259
Zouambia Sohaib <js_zouambia@esi.dz>
2022-10-01 18:22:30 +0100

    Added more spheres to the show

--- src/components/Sphere.jsx
+++ src/components/Sphere.jsx
@@ -19,13 +19,17 @@ function Sphere() {
 					position={[0, 0, 0]}
 					geometry={nodes.Cube008.geometry}
 					material={nodes.Cube008.material} />
-				<mesh visible position={[480, 0, 120]} scale={0.4}>
+				<mesh onClick={() => console.log("blue clicked")} visible position={[480, 0, 120]} scale={0.4}>
 					<sphereBufferGeometry args={[20, 20, 20]} />
 					<meshStandardMaterial color={"blue"} />
 				</mesh>
-				<mesh visible position={[480, 0, 120]} scale={0.4}>
+				<mesh onClick={() => console.log("red clicked")} visible position={[0, 480, 120]} rotation={[0, 0, 0]} scale={0.4}>
 					<sphereBufferGeometry args={[20, 20, 20]} />
-					<meshStandardMaterial color={"blue"} />
+					<meshStandardMaterial color={"red"} />
+				</mesh>
+				<mesh onClick={() => console.log("green clicked")} visible position={[0, 270, 420]} rotation={[0, 0, 0]} scale={0.4}>
+					<sphereBufferGeometry args={[20, 20, 20]} />
+					<meshStandardMaterial color={"green"} />
 				</mesh>
 			</group>
 		</>
