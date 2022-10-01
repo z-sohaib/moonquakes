4c0014449a8e2ea4aafa520fea1977c14919b3ae
YousraFarhani <jy_farhani@esi.dz>
2022-10-01 18:25:51 +0100

    yousra

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,19 @@
+import Typewriter from "typewriter-effect"
+
 function Welcome() {
 	return (
 		<div className="h-screen bg-moon flex justify-center items-center text-center">
-			<p className="px-4 py-2 text-[60px] text-white">LEARN MORE ABOUT MOONQUAKES</p>
+			 
+			<p className="px-2 py-1 text-[70px] font-aileron text-white"><Typewriter
+			options={{
+				strings: ["LEARN MORE ABOUT MOONQUAKES"],
+				autoStart: true,
+				loop: true,
+			}}
+			/>
+			</p>
 			<img src="/images/logo.svg" alt="spaceman" className="absolute bottom-8 right-4" />
 		</div>
 	)
 }
-
 export default Welcome
