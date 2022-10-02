0891377cb594a16111b89d1b4f24b0c58c3eb827
YousraFarhani <jy_farhani@esi.dz>
2022-10-02 07:39:36 +0100

    hey

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ function Sidebar() {
 	return (
 		
 		<div className="glass inline-flex flex-col justify-center items-center gap-y-16 py-8 px-4">
-			<Link to="/home"><img src="/icons/1.svg" alt="icon 1" /></Link>
+			<button class = "test-btn"><Link to="/home"><img src="/icons/1.svg" alt="icon 1" /></Link></button>
 			<Link to="/home/about"><img src="/icons/2.svg" alt="icon 2" /></Link>
 			<Link to="/home/play"><img src="/icons/3.svg" alt="icon 3" /></Link>
 			<Link to="/home/info"><img src="/icons/4.svg" alt="icon 4" /></Link>
